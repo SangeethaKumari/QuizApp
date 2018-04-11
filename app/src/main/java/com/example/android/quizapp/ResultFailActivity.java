ca2850043e2b5cb205acd0555a2db41bacc3acaf
sangeetha_gsk <sangeethakumari.g@gmail.com>
2018-04-10 22:41:41 -0700

    ADDED CODE FOR SUPPORT RETRY ACTION.

--- app/src/main/java/com/example/android/quizapp/ResultFailActivity.java
+++ app/src/main/java/com/example/android/quizapp/ResultFailActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.quizapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ResultFailActivity extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class ResultFailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_fail);
     }
+
+    public void retryQuiz(View view) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
